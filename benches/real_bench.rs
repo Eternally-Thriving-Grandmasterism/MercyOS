b58c820101df9d948b496b35b3c16395eff82768
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-01-09 21:49:55 -0500

    Update real_bench.rs

--- benches/real_bench.rs
+++ benches/real_bench.rs
@@ -1,9 +1,34 @@
-//! benches/real_bench.rs - MercyOS v2.0 Real Benchmarks Eternal
-use criterion::{criterion_group, criterion_main, Criterion};
+#![feature(test)]
 
-fn bench_falcon_sign(c: &mut Criterion) {
-    c.bench_function("falcon_sign_512", |b| b.iter(|| MercyFusion::new(MercyScheme::FalconCompact).sign(&[42u8; 64])));
+extern crate test;
+
+use test::Bencher;
+use mercyos::{MercyFusion, MercyScheme};
+
+#[bench]
+fn bench_falcon_keygen(b: &mut Bencher) {
+    b.iter(|| {
+        let _ = MercyFusion::new(MercyScheme::FalconCompact);
+    });
 }
 
-criterion_group!(benches, bench_falcon_sign);
-criterion_main!(benches);
+#[bench]
+fn bench_falcon_sign(b: &mut Bencher) {
+    let fusion = MercyFusion::new(MercyScheme::FalconCompact);
+    let pk = fusion.public_key().unwrap();
+    let msg = b"MercyOS benchmark eternal ⚡️";
+    b.iter(|| {
+        let _sig = fusion.sign(msg);
+    });
+}
+
+#[bench]
+fn bench_falcon_verify(b: &mut Bencher) {
+    let fusion = MercyFusion::new(MercyScheme::FalconCompact);
+    let pk = fusion.public_key().unwrap();
+    let msg = b"MercyOS benchmark eternal ⚡️";
+    let sig = fusion.sign(msg).unwrap();
+    b.iter(|| {
+        let _ = MercyFusion::verify_with_pk(MercyScheme::FalconCompact, &pk, msg, &sig);
+    });
+}
